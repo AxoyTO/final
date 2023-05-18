4c913021cbaf669164ef9c59314986bfd436180b
Oguz Aksoy <oguzhan.aksoy@1ci.com>
2023-05-18 11:27:57 +0300

    secret key

--- menu/config.py
+++ menu/config.py
@@ -1,5 +1,7 @@
 import os
+from secrets import token_hex
 
 class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', token_hex(16))
     SQLALCHEMY_DATABASE_URI = "sqlite:///db.sqlite3"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
