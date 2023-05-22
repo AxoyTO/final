f3fe0e99e0ca691981f16f8b8f9f3dd733adf549
Oguz Aksoy <oguzhan.aksoy@1ci.com>
2023-05-22 13:00:53 +0300

    fix and backup

--- create_tables.py
+++ create_tables.py
@@ -1,5 +1,5 @@
-from orders import create_app, db
-from menu.models import Order, User, Menu
+from menu import create_app, db
+from menu.models import Cart, User, Menu, Orders
 
 orders_app = create_app()
 with orders_app.app_context():
